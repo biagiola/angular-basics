acc67c42da3d520f69f6c00ec6728acc008c2d7c
David <41311088+biagiola@users.noreply.github.com>
2019-11-30 13:47:32 -0300

    Add files via upload

---
+++ src/app/ComponentOne/one.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'component-1', 
+  templateUrl: './one.component.html',
+  styleUrls: ['./one.component.css']
+})
+export class ComponentOne {
+  name: string = "Andrew"
+}
