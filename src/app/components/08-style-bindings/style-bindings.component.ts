42a9e8c4ca57d9c4d7817798805b2eaa9004d4cd
David <41311088+biagiola@users.noreply.github.com>
2019-12-02 17:46:13 -0300

    Add files via upload

---
+++ src/app/components/08-style-bindings/style-bindings.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-style-binding',
+  template: `<div>
+                        <h2 [style.color]="'orange'">Style Bindings 01</h2>
+                        <h2 [style.color]="hasError ? 'red' : 'green' ">{{ text }}</h2>
+                        <h2 [style.color]="highlightColor">Style Bindings 02</h2>
+                        <h2 [ngStyle]="titleStyles">Style Bindings 03</h2> 
+                    </div>`,
+  styles: [`
+    div{
+      background: #cdcdcd;
+      padding: 3rem;
+      text-align: center;
+      font-family: lato;
+    }
+    .text-success { color: green }
+    .text-danger { color: red }
+    .text-special { font-style: italic }
+  `]
+})
+export class StyleBinding {
+
+  public text: string = 'This is an success example test';
+  
+  public hasError = false;
+  public highlightColor = "pink";
+  
+  public titleStyles = {
+      color: 'blue',
+      fontStyle: 'italic'
+  }
+  
+
+}
