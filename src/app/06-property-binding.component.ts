cff5a9c70c625d1d8337c0a1a8a7aea87552aa20
David <41311088+biagiola@users.noreply.github.com>
2019-12-02 12:47:15 -0300

    Create 06-property-binding.component.ts

---
+++ src/app/06-property-binding.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-binding-property',
+  template: `<div>
+              <input [disabled]="isDesabled01" [id]="myId01" type="text" [value]="valueProperty01">
+              <input [disabled]="isDesabled02" id={{myId02}} type="text" value={{valueProperty02}}>
+            </div>`,
+  styles: [`
+    div{
+      background: #cdcdcd;
+      padding: 3rem;
+      text-align: center;
+      font-family: lato;
+    }
+  `]
+})
+export class BindingProperty {
+
+  public myId01: string = 'firstName';
+  public valueProperty01: string = 'Enter first name';
+  public isDesabled01: boolean = true;
+
+  public myId02: string = 'firstName';
+  public valueProperty02: string = 'Enter last name';
+  public isDesabled02: boolean = false;
+
+}
