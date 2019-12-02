5020357e8363628fef34ccb6d44d6e3b1bc738a4
David <41311088+biagiola@users.noreply.github.com>
2019-12-02 18:00:59 -0300

    event bindings added

---
+++ src/app/components/09-event-bindings/event-bindings.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-binding',
+  template: `<div>
+                        <h2>Welcome {{text}}</h2>
+                        <button (click)="onClick($event)">Greet01</button>
+                        {{ greeting }}
+                        <button (click)="greeting='Hi there' ">Greet02</button> 
+                        
+                    </div>`,
+  styles: [`
+    div{
+      background: #cdcdcd;
+      padding: 3rem;
+      text-align: center;
+      font-family: lato;
+    }
+  `]
+})
+export class EventBinding {
+
+  public text: string = 'Events Bindings';
+  public greeting ="";
+  
+  onClick(event) {
+      console.log(event);
+      this.greeting = "type of the event: " + event.type;
+  }
+
+}
