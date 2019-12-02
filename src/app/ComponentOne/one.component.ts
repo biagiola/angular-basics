eaea9de4d442931a6f47a00cf29ec14811361bc6
David <41311088+biagiola@users.noreply.github.com>
2019-12-02 11:42:14 -0300

    Update one.component.ts

--- src/app/ComponentOne/one.component.ts
+++ src/app/ComponentOne/one.component.ts
@@ -3,6 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'component-1', 
   templateUrl: './one.component.html',
+  /*
+    template: `<div>
+                Inline template
+              </div>`
+  */
   styleUrls: ['./one.component.css']
 })
 export class ComponentOne {
