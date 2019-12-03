9986784e58c7e4a2891fbabf7e1220c7dca05822
David <41311088+biagiola@users.noreply.github.com>
2019-12-03 11:48:01 -0300

    parentData added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
 
@@ -11,6 +13,14 @@ import { BoxThreeComponent } from './components/box-three/box-three.component';
 import { Interpolation } from './components/05-interpolation/interpolation.component';
 import { BindingProperty } from './components/06-property-binding/property-binding.component';
 import { ClassBinding } from './components/07-class-binding/class-binding.component';
+import { StyleBinding } from './components/08-style-bindings/style-bindings.component';
+import { EventBinding } from './components/09-event-bindings/event-bindings.component';
+import { TemplateReferenceVariables } from './components/10-template-reference-variables/template-reference-variables.component';
+import { TwoWayBinding } from './components/11-two-way-binding/two-way-binding.component';
+import { ngIfDirective } from './components/12-ng-if-directive/ng-if-directive';
+import { ngSwitchDirective } from './components/13-ng-switch-directive/ng-switch-directive.component';
+import { ngForDirective } from './components/14-ng-for-directive/ng-for-directive.component';
+import { ComponentInteraction } from './components/15-component-interaction/component-interaction.component';
 
 @NgModule({
   declarations: [
@@ -23,9 +33,19 @@ import { ClassBinding } from './components/07-class-binding/class-binding.compon
     Interpolation,
     BindingProperty,
     ClassBinding,
+    StyleBinding,
+    EventBinding,
+    TemplateReferenceVariables,
+    TwoWayBinding,
+    ngIfDirective,
+    ngSwitchDirective,
+    ngForDirective,
+    ComponentInteraction,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
