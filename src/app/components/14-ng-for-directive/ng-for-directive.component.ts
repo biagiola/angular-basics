3849a1e591b69660862649676c139b4e53e187f1
David <41311088+biagiola@users.noreply.github.com>
2019-12-03 11:15:47 -0300

    Add files via upload

---
+++ src/app/components/14-ng-for-directive/ng-for-directive.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-for-directives',
+  template: `<div>
+
+                        <h2>Welcome {{text}}</h2>
+                        
+                        <div *ngFor="let color of colors; index as i; first as f; second as s; last as l; odd as o; even as e">
+                            <h4> {{ i+1 }} - {{ color }} - first : {{ f }} - last: {{ l }} - odd: {{ o }} - even: {{ e }}</h4>
+                        </div>
+
+                    </div><br/>`,
+  styles: [`
+    div {
+      background: #cdcdcd;
+      padding: 3px;
+      text-align: center;
+      font-family: lato;
+    }
+  `]
+})
+export class ngForDirective {
+
+  public text: string = 'ng For Directives';
+  
+  public colors: string[] = ["red", "blue", "green", "yellow"];
+  
+} 
