82618a78ee000ce73bc2d5b36ad48c418923ee1e
David <41311088+biagiola@users.noreply.github.com>
2019-12-03 10:58:45 -0300

    Add files via upload

---
+++ src/app/components/13-ng-switch-directive/ng-switch-directive.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-switch-directives',
+  template: `<div>
+
+                        <h2>Welcome {{text}}</h2>
+
+                        <div [ngSwitch]="color">
+                            <div *ngSwitchCase=" 'red' ">You picked red color</div>
+                            <div *ngSwitchCase=" 'blue' ">You picked blue color</div>
+                            <div *ngSwitchCase=" 'green' ">You picked green color</div>
+                            <div *ngSwitchDefault>Pick again</div>
+                        </div>
+
+                    </div><br/>`,
+  styles: [`
+    div {
+      background: #cdcdcd;
+      padding: 1rem;
+      text-align: center;
+      font-family: lato;
+    }
+  `]
+})
+export class ngSwitchDirective {
+
+  public text: string = 'ng Switch Directives';
+  public color: string = "yellow";
+  
+} 
