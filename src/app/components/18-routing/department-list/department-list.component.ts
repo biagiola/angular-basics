e7b8fb6a4cd12a83bbc8098fa480d3821f100a65
David <41311088+biagiola@users.noreply.github.com>
2019-12-10 16:45:16 -0300

    department details added

--- src/app/components/18-routing/department-list/department-list.component.ts
+++ src/app/components/18-routing/department-list/department-list.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router} from '@angular/router';
 
 @Component({
   selector: 'app-department-list',
   template: `
     <div>Departments component works</div>
+    <ul>
+        <li (click)="onSelect(department)" *ngFor="let department of departments">
+            <span>{{ department.id }}</span> {{ department.name }}
+        </li>
+    </ul>
   `,
   styles: [``]
 })
-export class DepartmentListComponent {
+export class DepartmentListComponent implements OnInit {
+
+    departments = [
+        { "id": 1, "name": "Angular"},
+        { "id": 2, "name": "Node"},
+        { "id": 3, "name": "MongoDB"},
+        { "id": 4, "name": "Ruby"},
+        { "id": 5, "name": "Bootstrap"}
+    ]
+
+    constructor( private router: Router) {}
+
+    ngOnInit() {
+
+    }
+
+    onSelect(department) {
+        this.router.navigate(['/departments',department.id]);
+    }
 
 }
