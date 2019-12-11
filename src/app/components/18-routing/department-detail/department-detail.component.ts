2316aabbd584e94b3733ab460b2025053b320a1c
David <41311088+biagiola@users.noreply.github.com>
2019-12-11 14:21:56 -0300

    paramMap Observable added

--- src/app/components/18-routing/department-detail/department-detail.component.ts
+++ src/app/components/18-routing/department-detail/department-detail.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-department-detail',
   template: `    
     <h3>You selected department with id = {{departmentId}}</h3>
+    <a (click)="goPrevious()">Previous</a>
+    <a (click)="goNext()">Next</a>
   `,
   styles: []
 })
@@ -13,11 +15,37 @@ export class DepartmentDetailComponent implements OnInit {
 
   public departmentId: number;
 
-  constructor( private route: ActivatedRoute ) { }
+  constructor( private route: ActivatedRoute, private router: Router ) { }
 
   ngOnInit() {
-    let id = parseInt(this.route.snapshot.paramMap.get('id'));
-    this.departmentId = id;
+    //let id = parseInt(this.route.snapshot.paramMap.get('id'));
+    //this.departmentId = id;
+    /* 
+        snapshot has a problem with router specially when you wnat to
+        go back in a previous same component. 
+        Angular is going to figure out if he will initialize a new component or
+        if can re use it; when we navigate back to the same component angular
+        simply reuse the component, no initialization implies but ngOnit() 
+        method does not get call in enhance the new id will never be retrieved
+        from the url.
+    */
+   /*
+        the paramMap API provide us the get method to retrieve the id parameter   
+   */
+    this.route.paramMap.subscribe( (params: ParamMap) => {
+        let id = parseInt(params.get('id'));
+        this.departmentId = id;
+    });
+  }
+
+  goPrevious(){
+    let previousId = this.departmentId - 1;
+    this.router.navigate(['/departments', previousId]);
+  }
+
+  goNext(){
+    let nextId = this.departmentId + 1;
+    this.router.navigate(['/departments', nextId]);
   }
 
 }
